53cbcca2cf0f966653d5377043588a87eaed68a6
Fahersto <>
2021-07-10 17:38:24 +0200

    readding imgui

--- src/DrawHooks.cpp
+++ src/DrawHooks.cpp
@@ -3,9 +3,9 @@
 
 
 
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_win32.h"
-#include "../imgui/imgui_impl_dx11.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx11.h"
 
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
